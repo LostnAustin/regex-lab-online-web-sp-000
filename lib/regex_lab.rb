fd73801008dfbc920df49d9dec373c275b703fb1
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-20 17:17:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.length > 5
+  if string.length > 5
     return false
   else
     return true
