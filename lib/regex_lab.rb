678d0d998aa44c57dd759230b7c5a5e840a18ae8
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-20 17:10:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\w{5}\b/)
+  text.scan(/\w+{5}\b/)
   # ^[a-zA-Z]{5}$
 #  text.match(/[.....]/) == true
 
