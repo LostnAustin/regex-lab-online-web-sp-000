659f139221d4e6ebdcc25a8e9a99717a9e0b1ae5
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-20 19:55:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -32,7 +32,7 @@ end
 
 
 def valid_phone_number?(phone)
-    phone.scan(/^\W{5}-\d{6-10})
+    phone.scan(/^\W{5}-\d{6-10}/)
 #  phone.scan(/^\W{8}-\d{9-12}$\b/)
   #  phone.scan(/^\d{8}-\d{9-12}$\b/)
 
