eb87ecbf3f8d4c4a520eb167e514550da8684f7e
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-20 20:03:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -33,7 +33,7 @@ end
 
 def valid_phone_number?(phone)
     phone.scan(/^\W{6,8}-\W{3,5}$/)
-    #start of line/ any non-word character(length between 6-8/  - / any non-word character(length between 3,5))
+    #start of line/ any non-word character(length between 6-8/  - / any non-word character(length between 3,5)) / end of line
 #  phone.scan(/^\W{8}-\d{9-12}$\b/)
   #  phone.scan(/^\d{8}-\d{9-12}$\b/)
 
